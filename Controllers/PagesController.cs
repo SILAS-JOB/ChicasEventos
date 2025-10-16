4bd3c75e3be6bb22461eb47d7a421d01d2081a36
SILAS-JOB <tiilas777@gmail.com>
2025-10-16 07:21:17 -0300

    refact: escreveremos nessa base em asp net

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ChicasEventos.Models;
+
+namespace ChicasEventos.Controllers
+{
+    public class PagesController : Controller
+    {
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChicasEventos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
